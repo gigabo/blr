775bc599f843514ca11d97c28430e15751cd3121
Bo Borgerson <gigabo@gmail.com>
2010-09-11 13:29:10 -0700

    nested_packages.rb: Lexical scope can be re-opened
    This is interesting.  Re-opening a class definition actually re-opens
    the lexical scope as well.  A nested class definition has a different
    environment from a top-level definition of a class with a nested name,
    regardless of immediate context.

--- nested_packages.rb
+++ nested_packages.rb
@@ -4,6 +4,8 @@ class Outer
   OUT_OF_REACH = "Can't touch this!"
 end
 
+# This creates a class with a nested name, but the definiton
+# isn't in a nested lexical scope with respect to the Outer class
 class Outer::NestedNameOnly
   def reach_into_outer_guts
     OUT_OF_REACH
@@ -14,5 +16,19 @@ begin
   Outer::NestedNameOnly.new.reach_into_outer_guts
 
 rescue NameError => e
-  puts "Caught #{e.class}: #{e.message}"
+  puts "NestedNameOnly Caught #{e.class}: #{e.message}"
 end
+
+# This actually re-opens the lexical scope of the Outer class definition,
+# so the OUT_OF_REACH constant is actually available.
+class Outer
+  class NestedLexicalScope
+    def reach_into_outer_guts
+      OUT_OF_REACH
+    end
+  end
+end
+
+puts "NestedLexicalScope succeeded: #{
+  Outer::NestedLexicalScope.new.reach_into_outer_guts
+}"
